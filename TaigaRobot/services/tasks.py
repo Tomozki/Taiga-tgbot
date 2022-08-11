0cf1af3b26c0bf1f7e10e14bd9dab9ecc6438fac
Tomozaki <106297933+Tomozki@users.noreply.github.com>
2022-08-11 23:47:16 +0530

    Update tasks.py

--- TaigaRobot/services/tasks.py
+++ TaigaRobot/services/tasks.py
@@ -2,8 +2,8 @@ from asyncio import Lock, create_task
 from time import time
 from pyrogram import filters
 from pyrogram.types import Message
-from RiasGremoryBot import BOT_ID, BOT_USERNAME, DEV_USERS
-from RiasGremoryBot.services.sections import bold, section, w
+from TaigaRobot import BOT_ID, BOT_USERNAME, DEV_USERS
+from TaigaRobot.services.sections import bold, section, w
 tasks = {}
 TASKS_LOCK = Lock()
 arrow = lambda x: (x.text if x else "") + "\n`→`"
