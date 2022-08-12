8dd2e3ebfde0cc6a12a84208a1697e599d3bd65d
Tomozaki <106297933+Tomozki@users.noreply.github.com>
2022-08-12 17:01:52 +0530

    Update userinfo.py

--- TaigaRobot/modules/userinfo.py
+++ TaigaRobot/modules/userinfo.py
@@ -267,8 +267,6 @@ def info(update: Update, context: CallbackContext):
 
     if user.username:
         text += f"\n￫ Username: @{html.escape(user.username)}"
-     
-        text += f"\n￫ No. of pfp: {html.escape(user.dc_id)}"
 
     text += f"\n￫ Userlink: {mention_html(user.id, 'Sensei')}"
 
