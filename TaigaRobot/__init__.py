14f329247bedbb5bec8838d7bbddfc2bc78c1012
Vivek Singh <tomozakiop@gmail.com>
2023-05-02 18:21:23 +0530

    .

--- TaigaRobot/__init__.py
+++ TaigaRobot/__init__.py
@@ -101,23 +101,23 @@ if ENV:
     ARQ_API = os.environ.get("ARQ_API", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    #TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    #TEMP_DOWNLOAD_LOC = os.environ.get("TEMP_DOWNLOAD_LOC" , None)
-    OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
-    VIRUS_API_KEY = os.environ.get("VIRUS_API_KEY", None)
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
-    DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
-    STRICT_GMUTE = bool(os.environ.get("STRICT_GMUTE", True))
-    WORKERS = int(os.environ.get("WORKERS", 8))
-    BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
-    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
-    CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
-    TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
-    WALL_API = os.environ.get("WALL_API", None)
-    KAZUHA_ID = os.environ.get("KAZUHA_ID", 5358835742)
+    # HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    # HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    # #TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
+    # #TEMP_DOWNLOAD_LOC = os.environ.get("TEMP_DOWNLOAD_LOC" , None)
+    # OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
+    # VIRUS_API_KEY = os.environ.get("VIRUS_API_KEY", None)
+    # NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
+    # DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
+    # STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", False))
+    # STRICT_GMUTE = bool(os.environ.get("STRICT_GMUTE", True))
+    # WORKERS = int(os.environ.get("WORKERS", 8))
+    # BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
+    # ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+    # CASH_API_KEY = os.environ.get("CASH_API_KEY", None)
+    # TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
+    # WALL_API = os.environ.get("WALL_API", None)
+    # KAZUHA_ID = os.environ.get("KAZUHA_ID", 5358835742)
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
     L_CHAT = os.environ.get("L_CHAT", None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
@@ -194,31 +194,31 @@ else:
     #TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
     OPENWEATHERMAP_ID = Config.OPENWEATHERMAP_ID
     NO_LOAD = Config.NO_LOAD
-    HEROKU_API_KEY = Config.HEROKU_API_KEY
-    HEROKU_APP_NAME = Config.HEROKU_APP_NAME
+    # HEROKU_API_KEY = Config.HEROKU_API_KEY
+    # HEROKU_APP_NAME = Config.HEROKU_APP_NAME
     DEL_CMDS = Config.DEL_CMDS
-    STRICT_GBAN = Config.STRICT_GBAN
+    # STRICT_GBAN = Config.STRICT_GBAN
     WORKERS = Config.WORKERS
-    REM_BG_API_KEY = Config.REM_BG_API_KEY
+    # REM_BG_API_KEY = Config.REM_BG_API_KEY
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
-    CASH_API_KEY = Config.CASH_API_KEY
-    TIME_API_KEY = Config.TIME_API_KEY
-    WALL_API = Config.WALL_API
+    # CASH_API_KEY = Config.CASH_API_KEY
+    # TIME_API_KEY = Config.TIME_API_KEY
+    # WALL_API = Config.WALL_API
     SUPPORT_CHAT = Config.SUPPORT_CHAT
-    L_CHAT = Config.L_CHAT
+    # L_CHAT = Config.L_CHAT
     SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
     SPAMWATCH_API = Config.SPAMWATCH_API
     SESSION_STRING = Config.SESSION_STRING
     INFOPIC = Config.INFOPIC
-    ALIVE_MEDIA = Config.ALIVE_MEDIA
-    PM_MEDIA = Config.PM_MEDIA
-    G_MEDIA = Config.G_MEDIA
-    H_MEDIA = Config.H_MEDIA
-    BOT_USERNAME = Config.BOT_USERNAME
-    STRING_SESSION = Config.STRING_SESSION
-    LASTFM_API_KEY = Config.LASTFM_API_KEY
-    CF_API_KEY = Config.CF_API_KEY
+    # ALIVE_MEDIA = Config.ALIVE_MEDIA
+    # PM_MEDIA = Config.PM_MEDIA
+    # G_MEDIA = Config.G_MEDIA
+    # H_MEDIA = Config.H_MEDIA
+    # BOT_USERNAME = Config.BOT_USERNAME
+    # STRING_SESSION = Config.STRING_SESSION
+    # LASTFM_API_KEY = Config.LASTFM_API_KEY
+    # CF_API_KEY = Config.CF_API_KEY
 
     try:
         BL_CHATS = {int(x) for x in Config.BL_CHATS or []}
@@ -230,7 +230,7 @@ else:
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(KAZUHA_ID)
 DEV_USERS.add(OWNER_ID)
-REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
+# REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 #try:
 
