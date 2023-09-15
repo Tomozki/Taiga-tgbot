0b0e96edb041f9ad6e0a20592238d8179b342297
Vivek Katoch <tomozakiop@gmail.com>
2023-09-15 22:50:54 +0530

    Update misc.py #fixed

--- TaigaRobot/modules/misc.py
+++ TaigaRobot/modules/misc.py
@@ -10,7 +10,6 @@ from TaigaRobot.modules.helper_funcs.chat_status import user_admin
 from TaigaRobot.modules.disable import DisableAbleCommandHandler
 from TaigaRobot import (
     dispatcher,
-    WALL_API,
 )
 import requests as r
 import wikipedia
@@ -115,49 +114,6 @@ def wiki(update: Update, context: CallbackContext):
                 f"⚠ Error\n There are too many query! Express it more!\nPossible query result:\n{eet}"
             )
 
-
-@send_action(ChatAction.UPLOAD_PHOTO)
-def wall(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    msg = update.effective_message
-    msg_id = update.effective_message.message_id
-    args = context.args
-    query = " ".join(args)
-    if not query:
-        msg.reply_text("Please enter a query!")
-        return
-    caption = query
-    term = query.replace(" ", "%20")
-    json_rep = r.get(
-        f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
-    ).json()
-    if not json_rep.get("success"):
-        msg.reply_text("An error occurred!")
-
-    else:
-        wallpapers = json_rep.get("wallpapers")
-        if not wallpapers:
-            msg.reply_text("No results found! Refine your search.")
-            return
-        index = randint(0, len(wallpapers) - 1)  # Choose random index
-        wallpaper = wallpapers[index]
-        wallpaper = wallpaper.get("url_image")
-        wallpaper = wallpaper.replace("\\", "")
-        context.bot.send_photo(
-            chat_id,
-            photo=wallpaper,
-            caption="Preview",
-            reply_to_message_id=msg_id,
-            timeout=60,
-        )
-        context.bot.send_document(
-            chat_id,
-            document=wallpaper,
-            filename="wallpaper",
-            caption=caption,
-            reply_to_message_id=msg_id,
-            timeout=60,
-        )
 
 
 def helps(chat):
